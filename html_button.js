2a56b05640d6a8986f704d533aeb4c755e23dfd4
Eddy Parris <eddy@communecation.net>
2011-06-13 21:45:20 +0100

    modified:   html_button.js Added individual functions to handle mouseup, mousedown and click.

--- html_button.js
+++ html_button.js
@@ -8,30 +8,42 @@ ig.module(
   ig.Input.inject({
 
     // Overwrite bindTouch so we can bind click if touch is not available
-    bindTouch: function( selector, action ) {
+    bindTouch: function( selector, action_down, action_up, click ) {
+      var action_down = typeof(action_down) != 'undefined' ? action_down : false;
+      var action_up = typeof(action_up) != 'undefined' ? action_up : false;
+      var click = typeof(click) != 'undefined' ? click : false;
+      
   		var element = ig.$( selector );
 
   		var that = this;
       if('ontouchstart' in element && 'ontouchend' in element){
         console.log("binding touch");
     		element.addEventListener('touchstart', function(ev) {
-    			that.touchStart( ev, action );
+    			that.touchStart( ev, action_down );
     		}, false);
     		
     		element.addEventListener('touchend', function(ev) {
-    			that.touchEnd( ev, action );
+    			that.touchEnd( ev, action_down );
     		}, false);
   		}
   		else{
-  		  console.log("binding click");
-  		  element.addEventListener('mousedown', function(ev) {
-    			that.touchStart( ev, action );
-    		}, false);
+  		  if( action_down ){
+    		  element.addEventListener('mousedown', function(ev) {
+      			that.touchStart( ev, action_down );
+      		}, false);
+    		}
     		
-  		 
-    	  element.addEventListener('mouseup', function(ev) {
-    			that.touchStart( ev, action );
-    		}, false);
+  		  if( action_up ){
+      	  element.addEventListener('mouseup', function(ev) {
+      			that.touchStart( ev, action_up );
+      		}, false);
+    		}
+    		
+    		if( click ){
+    		  element.addEventListener('click', function(ev) {
+      			that.touchStart( ev, click );
+      		}, false);
+    		}
   		}
   	}
   	
